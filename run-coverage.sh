abf50d65aa8a81ee01f0573fe043578beca7718a
Tony Finn <tony@tonyfinn.com>
2020-04-15 22:22:05 +0100

    Switch to using `runtime-macros-derive`

--- run-coverage.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# test threads 1 needed because of tarpaulin segfault
-# https://github.com/xd009642/tarpaulin/issues/190
-cargo tarpaulin \
-    -o Html \
-    -- \
-    --test-threads 1 \
-
