a75c0e0bf452053dfb18f28a7ef4af6a67669443
Tony Finn <tony@tonyfinn.com>
2020-05-01 01:16:18 +0100

    Run tarpaulin from container cargo home (not cached cargo home)

--- build/test.sh
+++ build/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ ("$CI_COMMIT_BRANCH" = "$CI_DEFAULT_BRANCH") && ("x$COVERALLS_TOKEN" != "x") ]]; then
-    ~/.cargo/bin/cargo-tarpaulin --coveralls $COVERALLS_TOKEN
+    cargo tarpaulin --coveralls $COVERALLS_TOKEN
 else
-    ~/.cargo/bin/cargo-tarpaulin
+    cargo tarpaulin
 fi
