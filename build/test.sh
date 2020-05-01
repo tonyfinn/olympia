407da8851c6002bf2fa0ddc1e9ad18bf14efad2f
Tony Finn <tony@tonyfinn.com>
2020-05-01 02:02:38 +0100

    Update check for coveralls on master

--- build/test.sh
+++ build/test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-if [[ ("$CI_COMMIT_BRANCH" = "$CI_DEFAULT_BRANCH") && ("x$COVERALLS_TOKEN" != "x") ]]; then
+if [[ "$CI_COMMIT_BRANCH" = "master" ]]; then
+    echo "Coverage with sending to coveralls"
     cargo tarpaulin --coveralls $COVERALLS_TOKEN
 else
+    echo "Coverage without coveralls"
     cargo tarpaulin
 fi
