c62a788fb0f6c71dab8990d54e16d55255f9996e
Tony Finn <tony@tonyfinn.com>
2020-05-05 01:49:53 +0100

    Use git commands to set the right commit

--- build/test.sh
+++ build/test.sh
@@ -2,6 +2,8 @@
 
 if [[ "$CI_COMMIT_BRANCH" = "master" ]]; then
     echo "Coverage with sending to coveralls"
+    git checkout master
+    git reset --hard $CI_COMMIT_SHA
     cargo tarpaulin --coveralls $COVERALLS_TOKEN
 else
     echo "Coverage without coveralls"
