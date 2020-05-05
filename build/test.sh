d68a89da4fe348ea60feaee025cf7d64dd7ce6ff
Tony Finn <tony@tonyfinn.com>
2020-05-05 01:14:15 +0100

    Update HEAD for tarpaulin coveralls report

--- build/test.sh
+++ build/test.sh
@@ -2,6 +2,9 @@
 
 if [[ "$CI_COMMIT_BRANCH" = "master" ]]; then
     echo "Coverage with sending to coveralls"
+    # Set HEAD to master for tarpaulin coveralls report
+    git reset --soft master
+    git checkout master
     cargo tarpaulin --coveralls $COVERALLS_TOKEN
 else
     echo "Coverage without coveralls"
